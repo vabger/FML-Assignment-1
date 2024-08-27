9806f575c4b035c265f4055935567637ca296111
Vaibhav <vgera2001@gmail.com>
2024-08-27 10:33:31 +0530

    Given files

---
+++ part1B.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def initialise_input(N, d):
+  '''
+  N: Number of vectors
+  d: dimension of vectors
+  '''
+  np.random.seed(0)
+  U = np.random.randn(N, d)
+  M1 = np.abs(np.random.randn(d, d))
+  M2 = np.abs(np.random.randn(d, d))
+
+  return U, M1, M2
+
+def solve(N, d):
+  U, M1, M2 = initialise_input(N, d)
+
+  '''
+  Enter your code here for steps 1 to 6
+  '''
+  return max_indices
+  
+solve(N,d)
