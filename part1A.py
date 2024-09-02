29d5e27ae89c2fbe3efa1bcfce94cd078e12e66f
lakhanpal <lakhanpal185@gmail.com>
2024-09-02 11:39:56 +0530

    part1A.py done

--- part1A.py
+++ part1A.py
@@ -1,31 +1,37 @@
 from matplotlib import pyplot as plt
+from tqdm import tqdm 
 import numpy as np
 import random
-random.seed(45)
+import os
+
+
 
 num_coins = 100
 def toss(num_trials):
     '''
     num_trials: number of trials to be performed.
-    
     return a numpy array of size num_trials with each entry representing the number of heads found in each trial
-
     Use for loops to generate the numpy array and 'random.choice()' to simulate a coin toss
-    
     NOTE: Do not use predefined functions to directly get the numpy array. 
-    '''
+    '''  
     global num_coins
-    results = []
+   # results = []
     
     ## Write your code here
+    head_count = np.zeros(num_trials)
+    ishead = np.zeros(num_coins)
+    for j in tqdm(range(num_trials)):
+        for i in range(num_coins):
+            #if 1 then head
+             head_count[j]+=np.random.randint(0,2)
 
-    return results
+    return head_count
+    #return results
     
 
 def plot_hist(trial):
     '''
     trial: vector of values for a particular trial.
-     hi
     plot the histogram for each trial.
     Use 'axs' from plt.subplots() function to create histograms. You can search about how to use it to plot histograms.
 
@@ -34,6 +40,15 @@ def plot_hist(trial):
     fig, axs = plt.subplots(figsize =(10, 7), tight_layout=True)
     
     ## Write your code here
+    # Plot the histogram
+    axs.hist(trial, bins=range(num_coins + 2), edgecolor='black', alpha=0.7)
+    axs.set_title('Histogram of Number of Heads')
+    axs.set_xlabel('coins')
+    axs.set_ylabel('Number of Heads')
+
+    filename = f"hist_{len(trial)}.png"
+    fig.savefig( os.path.join('histograms', filename),dpi=300,bbox_inches="tight")
+
 
 if __name__ == "__main__":
     num_trials_list = [10,100,1000,10000,100000]
